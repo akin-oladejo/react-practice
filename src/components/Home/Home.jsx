f9d94d84cfa12fc21b7be9ad3ae89c70571f7801
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2024-02-26 00:02:52 +0100

    feat: add nav links to root header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,29 +3,29 @@ import { Link, NavLink } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-3xl my-4">Converting Designs using React</h1>
+    <div className="flex flex-col items-start px-10 my-10 ">
+      <h1 className="text-4xl my-4 font-bold">Converting Designs using React</h1>
 
-      <div className="my-2 flex flex-col items-center">
-        <h2 className="text-2xl">Single-page Projects</h2>
+      <div className="my-2 flex flex-col items-start">
+        <h2 className="text-3xl font-bold">Single-page Projects</h2>
         <NavLink to="/travel" className="text-blue-700 hover:text-blue-500">
-          Travel Page
+          1. Travel Page
         </NavLink>
         <NavLink to="/booking" className="text-blue-700 hover:text-blue-500">
-          Booking Services
+          2. Booking Services
         </NavLink>
       </div>
 
-      <div className="my-2 flex flex-col items-center">
-        <h2 className="text-2xl">Multi-page Projects</h2>
-        <div className="text-2">Squarespace</div>
-        <div className="text-2">Cordyline</div>
+      <div className="my-2 flex flex-col items-start">
+        <h2 className="text-3xl font-bold">Multi-page Projects</h2>
+        <div className="text-2">3. Squarespace</div>
+        <div className="text-2">4. Cordyline</div>
       </div>
 
-      <div className="my-2 flex flex-col items-center">
-        <h2 className="text-2xl">API Apps</h2>
-        <div className="text-2">RetroArt</div>
-        <div className="text-2">Vehicle HMI</div>
+      <div className="my-2 flex flex-col items-start">
+        <h2 className="text-3xl font-bold">API Apps</h2>
+        <div className="text-2">5. RetroArt</div>
+        <div className="text-2">6. Vehicle HMI</div>
       </div>
     </div>
   );
--- src/components/RootHeader.jsx
+++ src/components/RootHeader.jsx
@@ -1,8 +1,31 @@
-
 // className={`text-blue-700 ${({ isActive }) => isActive ? text-orange-700 : text-blue-700}`}
+
+import { NavLink } from "react-router-dom";
 
 export default function RootHeader() {
   return (
-    <div className='flex bg-slate-600'>rootHeader</div>
-  )
+    <div className="flex bg-black px-10 py-1 justify-center">
+      {/* nav: Travel */}
+      <NavLink
+        to="travel"
+        className={`hover:text-gray-200 text-white px-5 py-1.5 ${({
+          isActive,
+        }) => (isActive ? "text-yellow-300 border-white" : "text-red-500")}`}
+      >
+        Travel
+      </NavLink>
+
+      {/* nav: Booking */}
+      <NavLink
+        to="booking"
+        className={`hover:text-gray-200 text-white px-5 py-1.5 ${({
+          isActive
+        }) =>
+          isActive ? text-red-600 : ""}`
+        }
+      >
+        Booking
+      </NavLink>
+    </div>
+  );
 }
