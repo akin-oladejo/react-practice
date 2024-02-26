ad09702d5271925e5a214beca339da98c1750c72
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2024-02-26 16:57:53 +0100

    fix: change heart emoji on categorydiv

--- src/components/Bright/CategoryDiv.jsx
+++ src/components/Bright/CategoryDiv.jsx
@@ -8,8 +8,11 @@ function CategoryDiv({
 }) {
   return (
     <div className={`flex relative justify-center items-center col-start-${col} col-span-1 row-span-1 rounded-xl`} style={{backgroundImage: `url('${imgUrl}')`, backgroundSize: 'cover'}}>
-        <div className='absolute top-4 left-4 text-white'>Starting from ₦{startPrice.toLocaleString()}</div>
-        <img className='absolute top-4 right-4' width="48" height="48" src="https://img.icons8.com/color-glass/48/like--v1.png" alt="like--v1"/>
+        <div className='absolute top-0 p-2  backdrop-blur-[1px] flex justify-between w-full'>
+        <div className='left-4 text-white'>Starting from ₦{startPrice.toLocaleString()}</div>
+        <img className='right-4' width="30" height="30" src="https://img.icons8.com/pastel-glyph/64/like--v2.png" alt="like--v2"/>
+        </div>
+        
         <div className='text-3xl font-bold text-white'>{mainText}</div>
     </div>
   )
