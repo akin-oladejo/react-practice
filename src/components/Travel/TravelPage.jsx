7fc9ebf53c881ce8c99742b2cd7c1d604ae85a24
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2024-02-26 13:38:52 +0100

    fix: rearrange divs for responsiveness

--- src/components/Travel/TravelPage.jsx
+++ src/components/Travel/TravelPage.jsx
@@ -1,43 +1,72 @@
-import React from 'react'
-import TravelHeader from './TravelHeader'
-import TravelFooter from './TravelFooter'
+import React from "react";
+import TravelHeader from "./TravelHeader";
+import TravelFooter from "./TravelFooter";
 
 function TravelPage() {
   return (
     <>
-    <TravelHeader/>
-    {/* container */}
-    <div className='box-border flex object-center my-10 lg:max-h-60 lg:relative lg:left-72 items-center max-w-full'>
-
-      {/* left-side text */}
-      <div className="flex-col basis-1/3">
-        <h1 className='text-6xl font-bold my-7'>Travel</h1>
-        <p className='text-lg text-gray-600 font-semibold'>Discover exciting and latest world events, travel deals, safety tips and more news.</p>
-      </div>
-
-      {/* image gallery */}
-      <div className='flex-col grow'>
-        {/* buttons */}
-        <div className='flex w-36 justify-between py-2'>
-        <img width="30" height="30" className='cursor-pointer' src="https://img.icons8.com/ios-glyphs/30/circled-left-2.png" alt="circled-left-2"/>
-        <div className="text-5 bg-slate-200 w-7 h-7 text-center font-semibold cursor-pointer">01</div>
-        <div className="text-5 w-7 h-7 text-center cursor-pointer">02</div>
-        <div className="text-5 w-7 h-7 text-center cursor-pointer">03</div>
-        <img width="30" height="30" className='cursor-pointer scale-x-[-1]' src="https://img.icons8.com/ios-glyphs/30/circled-left-2.png" alt="circled-left-2"/>
+      <TravelHeader />
+      {/* container */}
+      <div className="box-border flex object-center my-10 lg:max-h-60 items-center max-w-full">
+        {/* left-side text */}
+        <div className="ps-48 basis-1/2">
+          <div className="flex-col me-10 w-prose">
+            <h1 className="text-6xl font-bold my-7">Travel</h1>
+            <p className="text-lg text-gray-600 font-semibold">
+              Discover exciting and latest world events, travel deals, safety
+              tips and more news.
+            </p>
+          </div>
         </div>
 
-        {/* images */}
-        <div className='flex max-w-full overflow-x-scroll'>
-          <img className='max-w-72 me-3' src="https://images.unsplash.com/photo-1708852154434-d6436655b99d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-          <img className='max-w-72 me-3' src="https://images.unsplash.com/photo-1708514349420-4eeb8397e733?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8" alt="" />
-          <img className='max-w-72 me-3' src="https://plus.unsplash.com/premium_photo-1708769592969-9f42825496a7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        {/* image gallery */}
+        <div className="flex-col">
+          {/* buttons */}
+          <div className="flex w-36 justify-between py-2">
+            <img
+              width="30"
+              height="30"
+              className="cursor-pointer"
+              src="https://img.icons8.com/ios-glyphs/30/circled-left-2.png"
+              alt="circled-left-2"
+            />
+            <div className="text-5 bg-slate-200 w-7 h-7 text-center font-semibold cursor-pointer">
+              01
+            </div>
+            <div className="text-5 w-7 h-7 text-center cursor-pointer">02</div>
+            <div className="text-5 w-7 h-7 text-center cursor-pointer">03</div>
+            <img
+              width="30"
+              height="30"
+              className="cursor-pointer scale-x-[-1]"
+              src="https://img.icons8.com/ios-glyphs/30/circled-left-2.png"
+              alt="circled-left-2"
+            />
+          </div>
+
+          {/* images */}
+          <div className="flex overflow-x-scroll basis-1/2">
+            <img
+              className="max-w-72 me-3"
+              src="https://images.unsplash.com/photo-1708852154434-d6436655b99d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <img
+              className="max-w-72 me-3"
+              src="https://images.unsplash.com/photo-1708514349420-4eeb8397e733?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8"
+              alt=""
+            />
+            <img
+              className="max-w-72 me-3"
+              src="https://plus.unsplash.com/premium_photo-1708769592969-9f42825496a7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <TravelFooter/>
+      <TravelFooter />
     </>
-    
-  )
+  );
 }
 
-export default TravelPage
+export default TravelPage;
