4776167bfaf50b00cbea9a36a328e5b9f5cb0638
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2024-02-26 12:21:50 +0100

    fix: add home link

--- src/components/RootHeader.jsx
+++ src/components/RootHeader.jsx
@@ -4,28 +4,38 @@ import { NavLink } from "react-router-dom";
 
 export default function RootHeader() {
   return (
-    <div className="flex bg-black px-10 py-1 justify-center">
-      {/* nav: Travel */}
+    <div className="flex bg-black px-10 py-1 justify-between ">
+      {/* nav: Home */}
       <NavLink
-        to="travel"
-        className={`hover:text-gray-200 text-white px-5 py-1.5 ${({
-          isActive,
-        }) => (isActive ? "text-yellow-300 border-white" : "text-red-500")}`}
+        to={"/"}
+        className={`hover:text-gray-200 text-white px-5 py-1.5 self-start`}
       >
-        Travel
+        Home
       </NavLink>
 
-      {/* nav: Bright */}
-      <NavLink
-        to="bright"
-        className={`hover:text-gray-200 text-white px-5 py-1.5 ${({
-          isActive
-        }) =>
-          isActive ? text-red-600 : ""}`
-        }
-      >
-        Bright
-      </NavLink>
+      <div className="flex px-5 py-1.5 justify-center">
+        {/* nav: Travel */}
+        <NavLink
+          to="travel"
+          className={`hover:text-gray-200 px-5 text-white ${({
+            isActive,
+          }) => (isActive ? "text-yellow-300 border-white" : "text-red-500")}`}
+        >
+          Travel
+        </NavLink>
+
+        {/* nav: Bright */}
+        <NavLink
+          to="bright"
+          className={`hover:text-gray-200 px-5 text-white ${({
+            isActive,
+          }) => (isActive ? text - red - 600 : "")}`}
+        >
+          Bright
+        </NavLink>
+      </div>
+
+      <div></div>
     </div>
   );
 }
